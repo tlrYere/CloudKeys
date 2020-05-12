d89d9fed0fca83a55a84b8e40bd93cbfb559fda4
tlrYere <tyler.yere772@myci.csuci.edu>
2020-05-11 19:45:44 -0700

    modified project to be deployable on AWS Elastic Beanstalk

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var ejs = require('ejs').__express;
 var app = express();
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var AWS = require('aws-sdk');
 
 //set credentials and region
 //AWS.config.update({region: process.env.region, credentials: {}})
+
+var tableName = "domain-passwords";
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 //Create application/x-www-forn-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({extended: false})
@@ -24,42 +26,46 @@ app.get('/', (req, res) => {
 
 app.post('/process_post', urlencodedParser, (req,res) => {
 	//Prepare output in JSON format
-	response = {
+	var response = {
 		Domain:req.body.Domain,
 		Password:req.body.Password
 	};
 	console.log(response);
 	
 	// write/update response in the "database"
-	var data = JSON.stringify(response);
-
-	fs.writeFileSync(__dirname + "/private/passwords/" + req.body.Domain + ".json", data);
+	var params = {
+		TableName: tableName,
+		Item: response
+	};
+	docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
 
 	res.end("Domain and password recieved by server");
 })
 
 app.get('/process_get', (req,res) => {
 	//want to search in the "database" for correct value
-	fs.readdir(__dirname + "/private/passwords/", (err, files) => {
-		files.forEach(file => {
-			if (file === (req.query.Domain + ".json"))
-			{
-				var rawdata = fs.readFileSync(__dirname + "/private/passwords/" + file);
-				var domainPasswordObject = JSON.parse(rawdata);
-				console.log(domainPasswordObject);
-
-				res.end(JSON.stringify(domainPasswordObject));
-			}
-		})
-		// if password not found, return this message
-		res.end("Password not found for specified domain");
+	
+	var params = {
+		TableName: tableName,
+		Key: {
+			"Domain": {"S": req.query.Domain}
 		}
-	)
+	};
+	docClient.query(params, function(err, data) {
+		if (err) res.end("Password not found for specified domain");
+		else res.end(JSON.stringify(data));
+	})
 })
 
-var server = app.listen(9001, () => {
+var server = app.listen(8080, () => {
 	var host = server.address().address
-	var port = server.address().port
+	var port = 8080
 
 	console.log("Application is listening at http://%s:%s", host, port)
 })
